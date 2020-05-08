18071626708050181c75fcc86aed71a9015720e9
Zohairajweh <60945291+Zohairajweh@users.noreply.github.com>
2020-05-08 13:31:16 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq; 
+using System.IO; 
+
+namespace C__Weekend_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Prompt: Please Enter Your file Name:");
+            string FileName = Console.ReadLine();
+            File.WriteAllText(FileName, "welcome"+ " "+FileName); 
+            Console.WriteLine("Your file name is "+ FileName);
+         
+
+        }
+    }
+}
