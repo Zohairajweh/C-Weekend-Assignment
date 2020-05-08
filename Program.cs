2f934fd931b839d38e7535469632fa2c245f6d23
Zohairajweh <60945291+Zohairajweh@users.noreply.github.com>
2020-05-08 16:05:03 -0700

    A while-loop menu

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Linq; 
-using System.IO; 
+using System.Linq;
+using System.IO;
 
 namespace C__Weekend_Assignment
 {
@@ -9,11 +9,36 @@ namespace C__Weekend_Assignment
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Prompt: Please Enter Your file Name:");
+            Console.WriteLine(" Please Enter Your file Name:");
             string FileName = Console.ReadLine();
-            File.WriteAllText(FileName, "welcome"+ " "+FileName); 
-            Console.WriteLine("Your file name is "+ FileName);
-         
+            File.WriteAllText(FileName, " ");
+            Console.WriteLine("Your file name is " + FileName);
+
+            Console.WriteLine("Split inside file");
+            string myContent = Console.ReadLine();
+            string[] myContentArray = myContent.Split(" ");
+            foreach (string element in myContentArray)
+            {
+                File.WriteAllText(FileName,element);
+                Console.WriteLine(element);
+
+            }
+
+
+        
+               int counter = 0; 
+
+                do { //A do while loop will ensure the code executes at least once before condition is checked
+                    counter++;
+                    Console.WriteLine(counter);
+                     if (counter == 3) {
+return;
+                } 
+                } 
+                
+                while (counter <3);
+
+              
 
         }
     }
